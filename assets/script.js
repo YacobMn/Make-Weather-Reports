94b6fb98bad62cdd0aa53cb58ab5c237fae1f342
unknown <yacobabr@gmail.com>
2023-10-30 09:45:05 -0500

    script

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
 var APIKey = `b969ba9ea8ffc35d20158e36d7f112db`
 var API = `https://api.openweathermap.org/data/2.5/forecast?q=${'minneapolis'}&appid=${APIKey}` 
+var fetchBtn = document.getElementsByClassName(".btn")
+var currentDate = document.getElementsByClassName(".date");
+
+
 
 fetch(API).then(function(res){
     return res.json()
 }).then(function(res){
-    console.log(res)
+    console.log(res.city)
+    
 })
+
+
+//fetchBtn.addEventListener('click', res.city);
+
