858b367cdf4389fb76aca24739aa0aaba7516d60
unknown <yacobabr@gmail.com>
2023-11-05 17:25:50 -0600

    lastone

--- assets/script.js
+++ assets/script.js
@@ -5,34 +5,13 @@ var tuesdayBtn = document.getElementsByClassName('.tuesday');
 var wednesdayBtn = document.getElementsByClassName('.wednesday');
 var thursdayBtn = document.getElementsByClassName('.thursday');
 var fridayBtn = document.getElementsByClassName('.friday');
-
+var searchFormEl = document.getElementById("searchForm");
 var APIKey = `b969ba9ea8ffc35d20158e36d7f112db`
-var API = `https://api.openweathermap.org/data/2.5/forecast?q=${'minneapolis'}&appid=${APIKey}` 
+ 
 var fetchBtn = document.getElementsByClassName(".btn")
 var currentDate = document.getElementsByClassName(".date");
 
 
-
-fetch(API).then(function(res){
-    return res.json()
-}).then(function(res){
-    console.log(res.city)
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i].name);
-        // target each row monday to friday
-        var todayDate = document.createElement('h3')
-        // one for date 
-        todayDate.textContent = res[i].city.date
-        // one for temp
-        issueTemp.textContent = res[i].city.temp
-        // one for wind 
-        issueWind.textContent = res[i].city.wind
-        // one for humidity
-        issueHumidity.textContent = res[i].city.wind
-
-      }
-    
-})
 
 //fetchBtn.addEventListener('click', res.city);
 function handleSearchSubmit(event) {
@@ -40,15 +19,49 @@ function handleSearchSubmit(event) {
     
     var searchInputVal = document.querySelector('#navebar').value;
     if (!searchInputVal) {
-        
-      return;
-    }else {
         alert('please enter a city')
+      return;
     }
   
-    var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-  
-    location.assign(queryString);
+    var API = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&appid=${APIKey}` 
+    var API2 = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputVal}&appid=${APIKey}`
+    fetch(API2).then(function(res){
+      return res.json()
+  }).then(function(res){
+      console.log(res)
+  })
+fetch(API).then(function(res){
+    return res.json()
+}).then(function(res){
+    console.log(res)
+    var cards = document.querySelector('.cards')
+        cards.innerHTML = ''
+    for (var i = 0; i < res.list.length; i+=8) {
+        console.log(res.list[i]);
+        
+        cards.innerHTML += `<div class="col-12 col-md-6 col-lg-2 mb-3 forecast-card tueday" style="max-width: 13rem ;">
+        <div class="card-body" style="background-color: #1e1e39;">
+          <h5 class="card-title" style="color: white;">${res.list[i].dt_txt}:</h5>
+          <p class="card-text" style="color: aliceblue;">Temp:${res.list[i].main.temp}</p>
+          <p class="card-text" style="color: aliceblue;">Wind:${res.list[i].wind.speed}</p>
+          <p class="card-text" style="color: aliceblue;">Humidity${res.list[i].main.humidity}</p>
+        </div>
+      </div>`
+        // target each row monday to friday
+        // var todayDate = document.createElement('h3')
+        // // one for date 
+        // todayDate.textContent = res.list[i].dt_text
+        // // one for temp
+        // issueTemp.textContent = res.list[i].main.temp
+        // // one for wind 
+        // issueWind.textContent = res.list[i].wind.speed
+        // // one for humidity
+        // issueHumidity.textContent = res.list[i].main.humidity
+
+      }
+    
+})
+
   }
   
   searchFormEl.addEventListener('submit', handleSearchSubmit);
